cb37035f74fe339c299ba6a706055c126213449b
Ejim-web <ejimchinedu24@gmail.com>
2026-05-02 12:14:39 +0100

    Add Login component for user authentication

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,1 +1,10 @@
-
+export default function Login() {
+  return (
+    <div>
+      <h2>Login to D-Bank</h2>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>Login</button>
+    </div>
+  );
+}
