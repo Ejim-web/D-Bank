4cf8a78e8a0bc82577682b48563073e8ac2aae86
Ejim-web <ejimchinedu24@gmail.com>
2026-05-02 12:19:06 +0100

    Add Transfer component for fund transfers

--- src/components/transfer.jsx
+++ src/components/transfer.jsx
@@ -1,1 +1,10 @@
-
+export default function Transfer() {
+  return (
+    <div>
+      <h2>Transfer Funds</h2>
+      <input type="text" placeholder="Recipient Account" />
+      <input type="number" placeholder="Amount" />
+      <button>Send Money</button>
+    </div>
+  );
+}
